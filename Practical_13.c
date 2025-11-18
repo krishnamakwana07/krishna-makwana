e870178c057913e20f03de69f555ba6f6850dcb8
krishnamakwana07 <makwanakrishna862@gmail.com>
2025-11-18 18:17:40 +0530

    Add files via upload

---
+++ Practical_13.c
@@ -0,0 +1,37 @@
+
+/*NAME : KRISHNA MAKWANA
+ ID   : 25CE056
+ AIM  : TO WRITE A C PROGRAM TO SIMULATE THE FILLING OF A WATER TANK OVER TIME USING A LOOP AND DELAY.
+*/
+
+
+
+#include<stdio.h>
+#include<unistd.h>
+
+    int main()
+
+    {
+        int t,r,a;
+
+        printf("Enter how much water is already in tank : ");
+        scanf("%d",&a);
+
+        printf("Enter tank refill Rate(Liters per Minute) : ");
+        scanf("%d",&r);
+
+        for(t=a;1<2;t=t+r)
+        {
+            if(t==100 || t>100)
+            {
+                printf("Tank is 100%% Full.\n\n");
+                return 0;
+            }
+
+            //printf("Current water level : %2d Liters\n",t);
+            printf("Tank is %3d%% Full.\n",t);
+            sleep(1);
+        }
+
+    return 0;
+    }
