66209bc6007cf71365a1dfd832010ac9bf9f5a27
krishnamakwana07 <makwanakrishna862@gmail.com>
2025-11-21 10:55:58 +0530

    Create pactical.24.c

---
+++ pactical.24.c
@@ -0,0 +1,177 @@
+
+/*
+NAME :: KRISHNA MAKWANA
+ROLL NO :: 25CE56
+AIM :: Write a C program to perform the following string operations without using built-in string functions:
+      a. Length of the string
+      b. Reverse of the string
+      c. Compare two strings
+      d. Copy one string to another
+      e. Concatenation of two strings
+      f. Convert to upper case
+      g. Convert to lower case
+      h. Capitalize each word in the string
+
+*/
+
+
+#include<stdio.h>          
+
+    void main()
+
+        {
+            char str1[]="Hello all";
+            char str2[]="Hi";
+            char str1rev[20];
+            char copy[20];
+            char Concat[20];
+            char upp[20];
+            char low[20];
+            char Cptl[20];
+            int i,j,length=0,flag=0;
+
+
+            //For calculating the Length of the String.
+
+            for(i=0;str1[i]!='\0';i++)
+            {
+                length++;
+            }
+            printf("1. The length of the given string is : %d\n\n",length);
+
+
+            //For reversing the String.
+
+            for(i=0;str1[i]!='\0';i++)
+            {
+                str1rev[i]=str1[(length-1)-i];
+            }
+            str1rev[i]='\0';
+
+            printf("2. Reverse : %s\n\n",str1rev);
+
+
+            //For comparing two Strings.
+
+            for(i=0 ; str1[i]!='\0' || str2[i]!='\0' ; i++)
+            {
+                if(str1[i]!=str2[i])
+                {
+                    flag=1;
+                    break;
+                }
+            }
+
+            if(flag==1)
+            {
+                printf("3. Strings are not Equal.\n\n");
+            }
+            else if(flag==0)
+            {
+                printf("3. Strings are Equal.\n\n");
+            }
+
+
+            //For copying the String.
+
+            for(i=0;str1[i]!='\0';i++)
+            {
+                copy[i]=str1[i];
+            }
+            copy[i]='\0';
+
+            printf("4. Copy : %s\n\n",copy);
+
+
+            //For Concatenation of Strings.
+
+            for(i=0;str1[i]!='\0';i++)
+            {
+                Concat[i]=str1[i];
+            }
+            Concat[i]=' ';
+            i++;
+
+            for(i=i,j=0 ; str2[j]!='\0' ; i++,j++)
+            {
+                Concat[i]=str2[j];
+            }
+            Concat[i]='\0';
+
+            printf("5. Concatenation of Strings : %s\n\n",Concat);
+
+
+            //For converting to upper case.
+
+            for(i=0;str1[i]!='\0';i++)
+            {
+                if(str1[i]>='a' && str1[i]<='z')
+                {
+                    upp[i]=str1[i]-32;  // to uppercase
+                }
+                else
+                {
+                    upp[i]=str1[i];
+                }
+            }
+            upp[i]='\0';
+            printf("6. Upper Case : %s\n\n",upp);
+
+
+            //For converting to lower case.
+
+            for(i=0;str1[i]!='\0';i++)
+            {
+                if(str1[i]>='A' && str1[i]<='Z')
+                {
+                    low[i]=str1[i]+32;  // to lowercase
+                }
+                else
+                {
+                    low[i]=str1[i];
+                }
+            }
+            low[i]='\0';
+            printf("7. Lower Case : %s\n\n",low);
+
+
+            //For Capitalize each word.
+
+            for(i=0;str1[i]!='\0';i++)
+            {
+                if(i==0)  // first character
+                {
+                    if(str1[i]>='a' && str1[i]<='z')
+                    {
+                        Cptl[i]=str1[i]-32;  // to uppercase
+                    }
+                    else
+                    {
+                        Cptl[i]=str1[i];
+                    }
+                }
+
+                else if(str1[i-1]==' ')  // after space
+                {
+                    if(str1[i]>='a' && str1[i]<='z')
+                    {
+                        Cptl[i]=str1[i]-32;  // to uppercase
+                    }
+                    else
+                    {
+                        Cptl[i]=str1[i];
+                    }
+                }
+
+                else
+                {
+                    Cptl[i]=str1[i];
+                }
+
+            }
+
+            Cptl[i]='\0';
+            printf("8. Capitalize : %s\n\n",Cptl);
+
+        }
+
