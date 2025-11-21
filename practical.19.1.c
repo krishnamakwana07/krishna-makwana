ef6ca5feb50ffdfd3bf615647d14d3a7d6e4b480
krishnamakwana07 <makwanakrishna862@gmail.com>
2025-11-21 10:40:50 +0530

    Add C program to print alternating 1s and 0s pattern
    This C program prints a pattern of alternating 1s and 0s for 5 lines.

---
+++ practical.19.1.c
@@ -0,0 +1,35 @@
+
+/*
+NAME:: KRISHNA MAKWANA
+ROLL NO:: 25CE56    
+AIM:: Write a C program to print the following pattern:
+     1
+     10
+     101
+     1010
+     10101
+*/
+
+#include<stdio.h>           
+
+    void main()
+
+        {
+            int i,j;
+
+            for(i=1;i<=5;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    if(j%2==0)
+                    {
+                        printf(" 0");
+                    }
+                    else
+                    {
+                    printf(" 1");
+                    }
+                }
+                printf("\n");
+            }
+        }
